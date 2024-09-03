f37c7eaad9580316cfede3d3b8a33c34c0d44e07
superten <superten@gmail.com>
2024-09-03 17:30:57 +0530

    final update

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ const name = "Super10";
 const dob = "19/01/2002";
 const id = "123";
 const isAdult = () => {
-    if (age >= 18)
-        return true;
-    else
-        return false;
+    return age >= 18;
 }
